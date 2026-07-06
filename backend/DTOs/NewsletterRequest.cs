fe6bd542d27f7f77937befbbc6824dae2843f110
Nicole <nicoledrawinghoe@gmail.com>
2026-07-06 17:39:05 +0200

    Added Backend.Tests project

--- backend/DTOs/NewsletterRequest.cs
+++ backend/DTOs/NewsletterRequest.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace Backend.DTOs;
 
 public record NewsletterRequest(
-    [Required(ErrorMessage ="Email address is required.")]
+    [Required(ErrorMessage = "Email address is required.")]
     [EmailAddress(ErrorMessage = "Given Email address is invalid.")]
     string Email
 );
