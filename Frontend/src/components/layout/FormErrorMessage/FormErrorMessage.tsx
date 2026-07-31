cbef758e2aa5ca63126503bb3d92e83deee12467
Nicole <nicoledrawinghoe@gmail.com>
2026-07-31 17:32:50 +0200

    Added motion animations for showing form errors

--- Frontend/src/components/layout/FormErrorMessage/FormErrorMessage.tsx
+++ Frontend/src/components/layout/FormErrorMessage/FormErrorMessage.tsx
@@ -1,17 +1,26 @@
 import styles from './FormErrorMessage.module.scss'
 import { CircleAlert } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
 
 interface FormErrorMessageProps {
 	errorMessage?: string
 }
 
 export default function FormErrorMessage({ errorMessage }: FormErrorMessageProps) {
-	if (!errorMessage) return null
-
 	return (
-		<div role='alert' className={styles['form-error-message']}>
-			<CircleAlert className={styles['form-error-message__icon']} />
-			<span className={styles['form-error-message__text']}>{errorMessage}</span>
-		</div>
+		<AnimatePresence>
+			{errorMessage && (
+				<motion.div
+					role='alert'
+					initial={{ opacity: 0, x: -10 }}
+					animate={{ opacity: 1, x: 0 }}
+					exit={{ opacity: 0, x: -10 }}
+					transition={{ duration: 0.2 }}
+					className={styles['form-error-message']}>
+					<CircleAlert className={styles['form-error-message__icon']} />
+					<span className={styles['form-error-message__text']}>{errorMessage}</span>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	)
 }
